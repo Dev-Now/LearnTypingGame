f8f1c6e6461879764e20827c91a9be8c35012719
Ghazi Majdoub <majdoub.ghazi@gmail.com>
2018-03-30 11:05:07 -0400

    completed core logic for read and display

--- LearnTypingGame/Program.cs
+++ LearnTypingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace LearnTypingGame
@@ -8,11 +9,36 @@ namespace LearnTypingGame
         static void Main(string[] args)
         {
             XDocument xDoc = XDocument.Load("DATA/all_exs.xml");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("THUNDERTYPER!");
 
-            Console.WriteLine(xDoc);
+            var qLvls = from c in xDoc.Elements().Elements("level") select c;
+            foreach (var itLvl in qLvls)
+            {
+                // Current level announcement
+                Console.WriteLine(itLvl.Element("title").Value);
 
-            Console.ReadLine();
+                var qParts = from c in itLvl.Elements("part") select c;
+                foreach (var itPart in qParts)
+                {
+                    // Current Part announcement
+                    if (itPart.Elements("title").Any()) { Console.WriteLine(itPart.Element("title").Value); }
+
+                    var qExs = from c in itPart.Element("exercices").Elements("ex") select c;
+                    foreach (var itEx in qExs)
+                    {
+                        // display current hint
+                        if (itEx.Elements("hint").Any()) { Console.WriteLine(itEx.Element("hint").Value); }
+                        // display challenge text
+                        if (itEx.Elements("text").Any()) { Console.WriteLine(itEx.Element("text").Value); }
+
+                        // Wait for input
+                        Console.ReadLine();
+                    }
+                }
+
+                Console.Clear();
+            }
+
         }
     }
 }
